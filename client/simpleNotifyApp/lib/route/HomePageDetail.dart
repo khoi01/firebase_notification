2caa0d5cc1ecce02e656dd735d672be715905bc3
Abdul Qayyum <khoi00091@gmail.com>
2020-12-31 16:13:41 +0800

    -not able to receive show notification when app in showing(foreground)
    -not able to receive show notification when app in showing(foreground)

--- client/simpleNotifyApp/lib/route/HomePageDetail.dart
+++ client/simpleNotifyApp/lib/route/HomePageDetail.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 class HomePageHeader extends StatelessWidget {
@@ -8,6 +9,58 @@ class HomePageHeader extends StatelessWidget {
     return Container(
       child: Text(
           "An Example of implement notification firebase from server (php) to client (flutter)"),
+    );
+  }
+}
+
+class HomePageContent extends StatefulWidget {
+  HomePageContent({Key key}) : super(key: key);
+
+  @override
+  _HomePageContentState createState() => _HomePageContentState();
+}
+
+class _HomePageContentState extends State<HomePageContent> {
+  var messageTitle = "Empty";
+
+  var notificationAlert = "alert";
+
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  @override
+  void initState() {
+    super.initState();
+
+    _firebaseMessaging.configure(
+      onMessage: (message) async {
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = "New Notification Alert";
+        });
+      },
+      onResume: (message) async {
+        setState(() {
+          messageTitle = message["data"]["title"];
+          notificationAlert = "Application opened from Notification";
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            notificationAlert,
+          ),
+          Text(
+            messageTitle,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ],
+      ),
     );
   }
 }
